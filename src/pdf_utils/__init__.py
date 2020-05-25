84e61e2d48408893b58769a09532ec559eb4a8c3
david <david@MSI>
2020-05-24 19:05:57 -0700

    added doc

--- src/pdf_utils/__init__.py
+++ src/pdf_utils/__init__.py
@@ -1,1 +1,12 @@
+"""
+Welcome to pdf_utils. The purpose of this package is to fix problems
+with scanned PDF documents. 
+
+**Note** - pdf_utils contains a module `PDF` which contains a class of
+the same name. In order to remove the extra '.PDF' from the module
+structure, this package imports the PDF class, so you can import the
+PDF class as `pdf_utils.PDF`. 
+"""
+
 from .PDF import PDF
+
